7a63569a134d5804fc95b2010925269d2759118b
Sérgio Moura <smouracalmon@gmail.com>
2022-11-17 12:24:48 -0300

    Alterando o status de uma tarefa

--- main.js
+++ main.js
@@ -51,16 +51,29 @@ const removerTarefa = (id) =>{
     liDaTarefa.remove();
 }
 
+const alterarTarefa = (id) => {
+    // Encontra a tarefa que tenha o id procurado
+    let tarefa = tarefas.find(t => t.id == id);
+
+    // Altera o status dessa tarefa
+    tarefa.feita = !tarefa.feita;
+
+    // Salvo o array de tarefas no localStorage
+    localStorage.setItem(chaveLocal, JSON.stringify(tarefas));
+}
+
 const showTarefa = (tarefa) => {
 
     // 4.1 Criar o novo li
     let li = document.createElement('li');
-    li.setAttribute('id',`li_${tarefa.id}`)
+    li.setAttribute('id',`li_${tarefa.id}`);
+
+    let checked = tarefa.feita ? 'checked' : '';
 
     // 4.2 Adicionar o conteúdo do li
     li.innerHTML = `
-        <input type="checkbox" id="check_1">
-        <label for="check_1">${tarefa.texto}</label>
+        <input type="checkbox" ${checked} id="check_${tarefa.id}" onclick="alterarTarefa(${tarefa.id})">
+        <label for="check_${tarefa.id}">${tarefa.texto}</label>
         <i class="material-icons" onclick="removerTarefa(${tarefa.id})">delete</i>
     `
 
